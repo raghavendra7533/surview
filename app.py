e8d81e9bebb35e92ea310fcf19f290474e3fd678
Raghavendra Radhakeishnan <radhakrishnannarasimhan@Radhakrishnans-Laptop.local>
2024-09-20 00:41:13 +0530

    Raghavs Changes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Required for sessions and flashing messages
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+OPENAI_API_KEY = ''
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 # User authentication decorator
